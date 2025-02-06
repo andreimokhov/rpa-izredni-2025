288b06a175cbec725cc7830bc94f9c56700b5066
amokhov <andrei.mokhov@scng.si>
2025-02-06 18:30:35 +0100

    Second Commit

---
+++ vaja-za-git/vaja-za-git/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaja_za_git {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Pozdravljen git!");
+            Console.ReadLine();
+        }
+    }
+}
